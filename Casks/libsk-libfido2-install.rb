fca3a1bb983f00ee9dfad02e91ac677315a526fd
MichaelRoosz <michael.roosz@web.de>
2023-09-29 13:01:50 +0200

    Update libsk-libfido2-install.rb

--- Casks/libsk-libfido2-install.rb
+++ Casks/libsk-libfido2-install.rb
@@ -12,5 +12,10 @@ cask "libsk-libfido2-install" do
   postflight do
     system_command "/bin/zsh", args: ["#{staged_path}/install-libsk-libfido2.zsh"], sudo: true
   end
+
+  uninstall delete: [
+    "/usr/local/lib/libsk-libfido2.dylib"
+  ],
+  sudo: true
   
 end
