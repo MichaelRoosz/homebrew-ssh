cf6f66db4482108f20468ed6f005e9cad200d9f1
MichaelRoosz <michael.roosz@web.de>
2023-09-29 11:22:16 +0200

    Update libsk-libfido2-install.rb

--- Casks/libsk-libfido2-install.rb
+++ Casks/libsk-libfido2-install.rb
@@ -2,30 +2,14 @@ cask "libsk-libfido2-install" do
   desc "libsk-libfido2 for MacOS Yubikey support for SSH"
   homepage "https://github.com/MichaelRoosz/homebrew-ssh/"
   version "9.4p1"
-
+  
+  url "https://raw.githubusercontent.com/MichaelRoosz/homebrew-ssh/main/etc/install-libsk-libfido2.zsh"
+  sha256 "0502670378365109175e09c645a6b7acdb743f9f9669c53335411a88c6efc84c"
+  
   depends_on arch: [:intel, :arm64]
   depends_on formula: "libsk-libfido2"
 
-  lib_path = "/usr/local/lib/libsk-libfido2.dylib"
-
-  preflight do
-    if Hardware::CPU.intel?
-      if File.symlink?(lib_path)
-        target = File.readlink(lib_path)
-        FileUtils.rm(lib_path)
-        FileUtils.cp(target, lib_path)
-      end
-    elsif Hardware::CPU.arm?
-      arm_lib_path = "/opt/homebrew/lib/libsk-libfido2.dylib"
-      if File.symlink?(arm_lib_path)
-        target = File.readlink(arm_lib_path)
-        FileUtils.rm(arm_lib_path)
-        FileUtils.cp(target, arm_lib_path)
-      end
-      FileUtils.cp(arm_lib_path, lib_path)
-    end
+  postflight do
+    system_command "/bin/zsh", args: ["#{staged_path}/install-libsk-libfido2.zsh"], sudo: true
   end
-
-  installer manual: "echo 'This cask modifies system files. No additional installation steps required.'"
-  
 end
