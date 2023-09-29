78f0e2954248275e37c366096c495132460273b7
MichaelRoosz <michael.roosz@web.de>
2023-09-29 13:43:46 +0200

    Update libsk-libfido2-install.rb

--- Casks/libsk-libfido2-install.rb
+++ Casks/libsk-libfido2-install.rb
@@ -3,14 +3,14 @@ cask "libsk-libfido2-install" do
   homepage "https://github.com/MichaelRoosz/homebrew-ssh/"
   version "9.4p1"
   
-  url "https://raw.githubusercontent.com/MichaelRoosz/homebrew-ssh/main/etc/install-libsk-libfido2-v2.zsh"
+  url "https://raw.githubusercontent.com/MichaelRoosz/homebrew-ssh/main/etc/install-libsk-libfido2.zsh"
   sha256 "4e8f6fd998b02064395acf7fe01adff4f5891926903cff0d049a95cd07c6434b"
   
   depends_on arch: [:intel, :arm64]
   depends_on formula: "michaelroosz/ssh/libsk-libfido2"
 
   postflight do
-    system_command "/bin/zsh", args: ["#{staged_path}/install-libsk-libfido2-v2.zsh"], sudo: true
+    system_command "/bin/zsh", args: ["#{staged_path}/install-libsk-libfido2.zsh"], sudo: true
   end
 
   uninstall_postflight do
