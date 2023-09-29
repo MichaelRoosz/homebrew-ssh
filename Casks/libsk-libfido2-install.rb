32e2d4a92df1bded2f209f8e607e4d1f8d552bf2
MichaelRoosz <michael.roosz@web.de>
2023-09-29 10:58:49 +0200

    Create libsk-libfido2-install.rb

---
+++ Casks/libsk-libfido2-install.rb
@@ -0,0 +1,36 @@
+cask "libsk-libfido2-install" do
+  desc "libsk-libfido2 for MacOS Yubikey support for SSH"
+  homepage "https://github.com/MichaelRoosz/homebrew-ssh/"
+  version "9.4p1"
+  license "SSH-OpenSSH"
+
+  livecheck do
+    url "https://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable/"
+    regex(/href=.*?openssh[._-]v?(\d+(?:\.\d+)+(?:p\d+)?)\.t/i)
+  end
+
+  auto_updates true
+
+  depends_on arch: [:intel, :arm64]
+  depends_on formula: "libsk-libfido2"
+
+  lib_path = "/usr/local/lib/libsk-libfido2.dylib"
+
+  preflight do
+    if Hardware::CPU.intel?
+      if File.symlink?(lib_path)
+        target = File.readlink(lib_path)
+        FileUtils.rm(lib_path)
+        FileUtils.cp(target, lib_path)
+      end
+    elsif Hardware::CPU.arm?
+      arm_lib_path = "/opt/homebrew/lib/libsk-libfido2.dylib"
+      if File.symlink?(arm_lib_path)
+        target = File.readlink(arm_lib_path)
+        FileUtils.rm(arm_lib_path)
+        FileUtils.cp(target, arm_lib_path)
+      end
+      FileUtils.cp(arm_lib_path, lib_path)
+    end
+  end
+end
