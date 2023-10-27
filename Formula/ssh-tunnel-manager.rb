e15f518a8f97b55d981a934e0ca6d0ef1b115793
michael.roosz <michael.roosz@check24.de>
2023-10-27 12:33:14 +0200

    ssh-tunnel-manager v1.0.10

--- Formula/ssh-tunnel-manager.rb
+++ Formula/ssh-tunnel-manager.rb
@@ -1,12 +1,12 @@
 class SshTunnelManager < Formula
   desc "SSH tunnel management tool impkemented with xbar"
   homepage "https://github.com/MichaelRoosz/ssh-tunnel-manager"
-  url "https://github.com/MichaelRoosz/ssh-tunnel-manager/archive/refs/tags/v1.0.8.tar.gz"
-  sha256 "bb01a9f8514a22f39371fa6480b98ab3ca4ea79e43c4b796e861394f1224ad1f"
+  url "https://github.com/MichaelRoosz/ssh-tunnel-manager/archive/refs/tags/v1.0.10.tar.gz"
+  sha256 "e2f0170f396884c7bc6cb73bc1f0778f16d07a5f283b4823e9746b4e97243908"
   license "GPL-2.0-or-later"
 
   depends_on "jq"
-  depends_on "openssh"
+  depends_on "openssh" => :optional
   depends_on "theseal/ssh-askpass/ssh-askpass"
   
   def install
