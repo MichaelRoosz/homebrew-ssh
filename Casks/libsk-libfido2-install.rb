829c600cd4a975be594511305931a709e3f462d7
michael.roosz <michael.roosz@check24.de>
2024-08-01 13:18:05 +0200

    Update libsk-libfido2-install

--- Casks/libsk-libfido2-install.rb
+++ Casks/libsk-libfido2-install.rb
@@ -1,7 +1,7 @@
 cask "libsk-libfido2-install" do
   desc "libsk-libfido2 for MacOS Yubikey support for SSH"
   homepage "https://github.com/MichaelRoosz/homebrew-ssh/"
-  version "9.5p1_build7"
+  version "9.5p1_build8"
   
   url "https://raw.githubusercontent.com/MichaelRoosz/homebrew-ssh/main/etc/install-libsk-libfido2-v1.1.3.zsh"
   sha256 "cc96f5a3f68d3adcb7ebea1bc4868f44982c840596bfbb69d3130ccf28c564ed"
@@ -13,7 +13,7 @@ cask "libsk-libfido2-install" do
   postflight do
     system_command "/bin/zsh", args: ["#{staged_path}/install-libsk-libfido2-v1.1.3.zsh"], sudo: true
     system_command "/bin/zsh", args: ["-c", "/bin/launchctl unload /Library/LaunchAgents/com.mroosz.ssh_env_vars.plist || true"], sudo: false
-    system_command "/bin/zsh", args: ["-c", "/bin/launchctl load /Library/LaunchAgents/com.mroosz.ssh_env_vars.plist"], sudo: false
+    system_command "/bin/zsh", args: ["-c", "/bin/launchctl load /Library/LaunchAgents/com.mroosz.ssh_env_vars.plist || true"], sudo: false
     system_command "/bin/zsh", args: ["-c", "echo 'export SSH_SK_PROVIDER=/usr/local/lib/libsk-libfido2.dylib' >> ~/.zshrc || true"], sudo: false
   end
 
