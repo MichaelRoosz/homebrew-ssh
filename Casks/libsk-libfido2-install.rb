5832044e47212779561b99fc08427ddcb7daab1f
Nikolai Blank <nikolai.blank@check24.de>
2026-09-08 11:22:25 +0200

    Update cask install step stanzas

--- Casks/libsk-libfido2-install.rb
+++ Casks/libsk-libfido2-install.rb
@@ -10,14 +10,14 @@ cask "libsk-libfido2-install" do
   depends_on formula: "michaelroosz/ssh/libsk-libfido2"
   depends_on formula: "theseal/ssh-askpass/ssh-askpass"
 
-  postflight do
+  postflight_steps do
     system_command "/bin/zsh", args: ["#{staged_path}/install-libsk-libfido2-v1.1.6.zsh"], sudo: true
     system_command "/bin/zsh", args: ["-c", "/bin/launchctl unload /Library/LaunchAgents/com.mroosz.ssh_env_vars.plist &>/dev/null || true"], sudo: false
     system_command "/bin/zsh", args: ["-c", "/bin/launchctl load /Library/LaunchAgents/com.mroosz.ssh_env_vars.plist || true"], sudo: false
     system_command "/bin/zsh", args: ["-c", "echo 'export SSH_SK_PROVIDER=/usr/local/lib/libsk-libfido2.dylib' >> ~/.zshrc || true"], sudo: false
   end
 
-  uninstall_postflight do
+  uninstall_postflight_steps do
     system_command "/bin/zsh", args: ["-c", "rm /usr/local/lib/libsk-libfido2.dylib || true"], sudo: true
     system_command "/bin/zsh", args: ["-c", "/bin/launchctl unload /Library/LaunchAgents/com.mroosz.ssh_env_vars.plist || true"], sudo: false
     system_command "/bin/zsh", args: ["-c", "rm /Library/LaunchAgents/com.mroosz.ssh_env_vars.plist || true"], sudo: true
